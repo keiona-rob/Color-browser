2d418e77feff010000cc025779ea0ab9761b68c9
Juzmekeke <keerob17@gmail.com>
2025-07-07 20:06:52 -0500

    made div of random color

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-async function fetchImages() {
+async function fetchColors() {
 
     try {
-        // render();
+        
         const response = await fetch("https://www.thecolorapi.com/random");
       if (!response.ok) {
       throw new Error("Network error", response.status);
     } 
      const data = await response.json();
      console.log(data);
-     gallery.innerHTML = data
+    
      return data;
      } catch (error) {
     console.log("ERROR Fetching by color data ", error.message);
     } finally {
     console.log("Finished fetching color data");
     }
+   
 }
 // console.log(Error);
-fetchImages()
+
+showColors()
+function render(data){
+const paletteElem = document.getElementById("palette");
+paletteElem.style.backgroundColor = data.hex.value
+}
+
+async function showColors(){
+    const data = await fetchColors();
+    render(data);
+}
