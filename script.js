453ad5507c91445cd3d584f9a375d506f47fd12e
Juzmekeke <keerob17@gmail.com>
2025-07-10 03:38:46 -0500

    made 5 colors generate

--- script.js
+++ script.js
@@ -36,16 +36,60 @@ const numValue2 = document.getElementById("hex-id2")
 paletteElem2.style.backgroundColor = data.hex.value;
 numValue2.innerHTML = data.hex.value;
 }
+function render3(data){
+const paletteElem3 = document.getElementById("palette3");
+const numValue3 = document.getElementById("hex-id3")
+paletteElem3.style.backgroundColor = data.hex.value;
+numValue3.innerHTML = data.hex.value;
+}
+function render4(data){
+const paletteElem4 = document.getElementById("palette4");
+const numValue4 = document.getElementById("hex-id4")
+paletteElem4.style.backgroundColor = data.hex.value;
+numValue4.innerHTML = data.hex.value;
+}
+function render5(data){
+const paletteElem5 = document.getElementById("palette5");
+const numValue5 = document.getElementById("hex-id5")
+paletteElem5.style.backgroundColor = data.hex.value;
+numValue5.innerHTML = data.hex.value;
+}
 
 // fetchColors and render(data) called in the showColors function
 
+async function showColors(color){
+    const data = await fetchColors()
+    render(data)
+};
 
+async function showColors2(color){
+    const data = await fetchColors();
+    render2(data);
+};
+
+async function showColors3(color){
+    const data = await fetchColors();
+    render3(data);
+};
+
+async function showColors4(color){
+    const data = await fetchColors();
+    render4(data);
+};
+
+async function showColors5(color){
+    const data = await fetchColors();
+    render5(data);
+};
 
 const generateButton = document.getElementById("generate-btn");
 
-generateButton.addEventListener("click", async function showColors(color){
-    const data = await fetchColors();
-    render(data);
+generateButton.addEventListener("click", async function showAll(){
+    showColors();
+    showColors2();
+    showColors3();
+    showColors4();
+    showColors5();
 });
 
 // let favorites = [];
