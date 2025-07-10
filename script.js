54f2946c8f6dd5026168354302e6f542a7bca2eb
Juzmekeke <keerob17@gmail.com>
2025-07-10 03:18:54 -0500

    java comments

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-async function fetchColors() {
+const state = {
+    currentColor: ""
+};
 
+async function fetchColors(color) {
+    state.currentColor = color
     try {
         
         const response = await fetch("https://www.thecolorapi.com/random");
@@ -24,17 +28,45 @@ function render(data){
 const paletteElem = document.getElementById("palette");
 const numValue = document.getElementById("hex-id")
 paletteElem.style.backgroundColor = data.hex.value;
-numValue.innerHTML = data.hex.value
+numValue.innerHTML = data.hex.value;
 }
+function render2(data){
+const paletteElem2 = document.getElementById("palette2");
+const numValue2 = document.getElementById("hex-id2")
+paletteElem2.style.backgroundColor = data.hex.value;
+numValue2.innerHTML = data.hex.value;
+}
+
+// fetchColors and render(data) called in the showColors function
+
+
 
 const generateButton = document.getElementById("generate-btn");
 
-generateButton.addEventListener("click", async function showColors(){
+generateButton.addEventListener("click", async function showColors(color){
     const data = await fetchColors();
     render(data);
 });
 
-// generateButton.addEventListener("click", function() {
+// let favorites = [];
+// const favoriteButton = document.getElementById("favorite-button");
 
-//  ;
-// });
+// const faveListElement = document.createElement("ul");
+
+// favoriteButton.addEventListener("click", async function addToFavorites(){
+//     const currentColor = document.createElement("li");
+    
+//     const favoriteColor = await 
+// favorites.push(currentColor);
+ 
+//     faveListElement.appendChild(favoriteColor);
+
+// }
+//  );
+
+//  create state
+
+
+
+
+ 
